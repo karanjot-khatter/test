f9247a0f5792ba8c760ec3214d6b6ded3f64c9e0
karanjot-khatter <karanjot_khatter@hotmail.co.uk>
2017-10-01 18:37:09 +0100

    5
    5

--- ascending.py
+++ ascending.py
@@ -12,10 +12,11 @@
 import operator
 import csv
 
-data = csv.reader(open("data.csv"), delimiter=",")
+data = csv.reader(open("data.csv"))
 
 for line in sorted(data, key=operator.itemgetter(10), reverse=True):
     print(line)
+
 
 
 
