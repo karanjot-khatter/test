0205e307abb1464383fac17ad8a551cd8f741f7e
karanjot-khatter <karanjot_khatter@hotmail.co.uk>
2017-10-01 15:44:20 +0100

    uu
    uu

---
+++ ascending.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Karanjot
+#
+# Created:     01/10/2017
+# Copyright:   (c) Karanjot 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import operator
+import csv
+
+data = csv.reader(open("data.csv"), delimiter=",")
+
+for line in sorted(data, key=operator.itemgetter(10), reverse=True):
+    print(line)
+
+
+
