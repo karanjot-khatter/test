efa75f3028ca317ee111ff7a6737b225ec75f80a
karanjot-khatter <karanjot_khatter@hotmail.co.uk>
2017-10-01 18:58:52 +0100

    a
    a

--- ascending.py
+++ ascending.py
@@ -1,20 +1,9 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      Karanjot
-#
-# Created:     01/10/2017
-# Copyright:   (c) Karanjot 2017
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
-
 import operator
 import csv
 
 data = csv.reader(open("data.csv"))
 
-for line in sorted(data,10, key=operator.itemgetter(10), reverse=True):
+for line in sorted(data, key=operator.itemgetter(10), reverse=True):
        print(line)
 
 
