f6dd19c464ef2d051c307a956a10fdfc2e5bd287
karanjot-khatter <karanjot_khatter@hotmail.co.uk>
2017-10-02 19:59:32 +0100

    ascneding
    ascending

--- ascending.py
+++ ascending.py
@@ -5,8 +5,8 @@ outFileName = "data2.csv"
 outFile = open(outFileName, "w")
 
 data = csv.reader(open("data.csv"), delimiter=',')
-
-for line in sorted(data, key=operator.itemgetter(10), reverse=True):
+for line in range(5):
+    for line in sorted(data, key=operator.itemgetter(10), reverse=True):
        print(line, file = outFile)
 
 outFile.close()
