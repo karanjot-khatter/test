cf33b1671610abd098a13faa883be3d50cbd4fd9
karanjot-khatter <karanjot_khatter@hotmail.co.uk>
2017-10-02 17:56:04 +0100

    gdegd
    gdgddg

--- ascending.py
+++ ascending.py
@@ -1,11 +1,14 @@
 import operator
 import csv
 
-data = csv.reader(open("data.csv"))
+outFileName = "data2.csv"
+outFile = open(outFileName, "w")
+
+data = csv.reader(open("data.csv"), delimiter=',')
 
 for line in sorted(data, key=operator.itemgetter(10), reverse=True):
-       print(line)
+       print(line.strip(), file = outFile)
 
-
+outFile.close()
 
 
